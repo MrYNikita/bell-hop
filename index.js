4efdfc49347e5c4b246481f5e1ba634ccde308f3
Nikita Yakhin <mr.y.nikita@gmail.com>
2024-08-18 22:05:05 +0500

    #7 Changed class order, added styles

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ class BellhopElement extends HTMLElement {
 };
 
 /**
- * Bellhop element class.
+ * Bellhop nav button class.
  * @class
 */
-class Bellhop extends BellhopElement {
-  static _tag = 'bell-hop';
+class BellButton extends BellhopElement {
+  static _tag = 'bell-button';
   constructor() {
     super();
   };
@@ -40,11 +40,45 @@ class BellPoint extends BellhopElement {
 };
 
 /**
- * Bellhop nav button class.
+ * Bellhop element class.
  * @class
 */
-class BellButton extends BellhopElement {
-  static _tag = 'bell-button';
+class Bellhop extends BellhopElement {
+  static _tag = 'bell-hop';
+  
+  static {
+    const style = document.createElement('style');
+    document.head.append(style);
+    style.innerHTML = `
+      ${BellPoint._tag} {
+        &[active] {
+          & > ${BellPoint._tag} {
+            display: none;
+          }
+        }
+        &:not([active], :has([active])) {
+          display: none;
+        }
+        &:not([active]):has([active]) {
+          & > :not(${BellPoint._tag}) {
+            display: none;
+          }
+        }
+      }
+
+      ${Bellhop._tag},
+      ${Bellhop._tag} ${BellPoint._tag} {
+        & {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    `;
+  };
+
   constructor() {
     super();
   };
