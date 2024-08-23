0fe88501aa5b5bfe2372f35768e81a2a56bdc705
Nikita Yakhin <mr.y.nikita@gmail.com>
2024-08-23 12:41:11 +0500

    Revert "#10 implement an event model for transitions between endpoints"

--- type.ts
+++
@@ -1,8 +0,0 @@
-/** Type describing events in bellhop. */
-export type TBellEven = {
-  act: (self: BellPoint) => void;
-  cond?: (self: BellPoint) => boolean;
-  name?: string;
-  delt?: boolean;
-  once?: boolean;
-};
